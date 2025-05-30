e55fed746b59d37238109425d4a980810eee79e4
Garry <mrgarrysingh92@gmail.com>
2025-05-30 16:16:02 +1000

    Add google search console meta tag

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="_W7qsE12Njou18Gc_VU9ygWW4vfGPdE1c2E_IcrEIi4"
+      />
+    </>
+  );
+}
